310bf520d2d66496cb210691d9a60872fb31beda
Richard <rchrdperreault@gmail.com>
2026-07-21 05:49:00 -0400

    Created a reset-build.bat file

---
+++ reset-build.bat
@@ -0,0 +1,26 @@
+﻿@echo off
+echo ============================================
+echo   Project - Clean & Rebuild
+echo ============================================
+
+echo.
+echo Deleting bin and obj folders...
+for /d /r %%i in (bin,obj) do (
+    echo Removing: %%i
+    rmdir /s /q "%%i"
+)
+
+echo.
+echo Running dotnet clean...
+dotnet clean
+
+echo.
+echo Running dotnet build...
+dotnet build
+
+echo.
+echo ============================================
+echo   Build complete!
+echo ============================================
+pause
+
